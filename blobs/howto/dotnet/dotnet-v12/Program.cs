a73d09e75e363be31e4a9ea80ab188b7fe3e9a68
normesta <normesta@microsoft.com>
2020-04-22 15:51:40 -0700

    Checking in a proposed stub project

---
+++ blobs/howto/dotnet/dotnet-v12/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace dotnet_v12
+{
+    class Program
+    {      
+        //-----------------------------------------------
+        // Submenu for diagnostic log scenarios.
+        //----------------------------------------------- 
+        static bool DiagnosticLogs()
+        {
+            DiagnosticLogs diagnosticLogs = new DiagnosticLogs();
+
+            while (diagnosticLogs.Menu()){}
+
+            return true;
+        }        
+        
+       //------------------------------------------------
+       // Main function
+       //------------------------------------------------
+        static void Main(string[] args)
+        {
+            while (MainMenu()){}
+        }
+
+        //-----------------------------------------------
+        // Main menu
+        //-----------------------------------------------
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose a feature area:");
+            Console.WriteLine("1) Diagnostic logs (classic)");
+            Console.WriteLine("2) Feature area 2");
+            Console.WriteLine("3) Feature area 3");
+            Console.WriteLine("4) Exit");
+            Console.Write("\r\nSelect an option: ");
+ 
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    return DiagnosticLogs();
+                case "2":
+                   return true;               
+                case "3":
+                   return true;
+                case "4":
+                   return false;                
+                default:
+                   return true;
+            }
+        }
+    
+    }    
+}
---
+++ blobs/howto/dotnet/dotnet-v12/diagnostic-logs-classic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace dotnet_v12
+{
+    public class DiagnosticLogs
+    {
+        //-------------------------------------------------
+        // Diagnostic log menu
+        //-------------------------------------------------
+        public bool Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose a diagnostic log scenario:");
+            Console.WriteLine("1) Scenario 1");
+            Console.WriteLine("2) Scenario 2");
+            Console.WriteLine("3) Return to main menu");
+            Console.Write("\r\nSelect an option: ");
+ 
+            switch (Console.ReadLine())
+            {
+                case "1":
+                   Console.WriteLine("You chose scenario 1. Press enter to continue");   
+                   Console.ReadLine();          
+                   return true;
+                case "2":
+                   Console.WriteLine("You chose scenario 2. Press enter to continue"); 
+                   Console.ReadLine();              
+                   return true;
+                case "3":
+                   return false;
+                default:
+                   return true;
+            }
+        }
+        
+    }
+
+    
+
+
+    
+}
