fdde2e8c7d2c445794f3dbc06923bb13e420a09f
normesta <normesta@microsoft.com>
2020-04-23 12:11:00 -0700

    Updating files

--- blobs/howto/dotnet/dotnet-v12/diagnostic-logs-classic.cs
+++ blobs/howto/dotnet/dotnet-v12/diagnostic-logs-classic.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿//----------------------------------------------------------------------------------
+// Microsoft Developer & Platform Evangelism
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+//----------------------------------------------------------------------------------
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+//----------------------------------------------------------------------------------
+
+using System;
 
 namespace dotnet_v12
 {
     public class DiagnosticLogs
     {
+        
+        //-------------------------------------------------
+        // Diagnostic logs snippet 1
+        //-------------------------------------------------
+
+        public void ExampleSnippet1(){
+
+            Console.WriteLine("Snippet code goes for Example 1 goes in here");
+        }
+
+        //-------------------------------------------------
+        // Diagnostic logs snippet 2
+        //-------------------------------------------------
+
+        public void ExampleSnippet2(){
+
+            Console.WriteLine("Snippet code goes for Example 2 goes in here");
+        }
+
         //-------------------------------------------------
         // Diagnostic log menu
         //-------------------------------------------------
+        
         public bool Menu()
         {
             Console.Clear();
@@ -19,16 +55,25 @@ namespace dotnet_v12
             switch (Console.ReadLine())
             {
                 case "1":
-                   Console.WriteLine("You chose scenario 1. Press enter to continue");   
+                   
+                   ExampleSnippet1();
+                   Console.WriteLine("Press enter to continue");   
                    Console.ReadLine();          
                    return true;
+                
                 case "2":
-                   Console.WriteLine("You chose scenario 2. Press enter to continue"); 
+                
+                   ExampleSnippet2();
+                   Console.WriteLine("Press enter to continue"); 
                    Console.ReadLine();              
                    return true;
+                
                 case "3":
+                
                    return false;
+                
                 default:
+                
                    return true;
             }
         }
