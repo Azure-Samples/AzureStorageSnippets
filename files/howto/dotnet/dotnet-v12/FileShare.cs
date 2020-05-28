6bc0dabae96b7cd1243323526577fd2f6f84cf72
Mark Hopkins <mhopkins@microsoft.com>
2020-05-27 17:29:31 -0700

    Updates

--- files/howto/dotnet/dotnet-v12/FileShare.cs
+++ files/howto/dotnet/dotnet-v12/FileShare.cs
@@ -18,9 +18,11 @@
 using System;
 using System.Configuration;
 using System.IO;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using Azure;
 using Azure.Storage;
+using Azure.Storage.Blobs;
 using Azure.Storage.Files.Shares;
 using Azure.Storage.Files.Shares.Models;
 using Azure.Storage.Sas;
@@ -120,6 +122,8 @@ namespace dotnet_v12
             // Ensure that the share exists
             if (await share.ExistsAsync())
             {
+                Console.WriteLine($"Share exists: {share.Uri}");
+
                 // Get and display current usage stats for the share
                 ShareStatistics stats = await share.GetStatisticsAsync();
                 Console.WriteLine($"Current share usage: {stats.ShareUsageInBytes} bytes");
@@ -143,62 +147,169 @@ namespace dotnet_v12
         public async Task GenerateSASAsync()
         {
             // <snippet_GenerateSAS>
+            Uri acctSasUri = CreateAccountSasUri();
+
+            // Create a ShareServiceClient which will be used to get a ShareClient
+            ShareServiceClient service = new ShareServiceClient(acctSasUri);
+
+            // Create a client that can authenticate with the SAS URI
+            ShareClient share = service.GetShareClient("logs");
+
+            if (await share.ExistsAsync())
+            {
+                Console.WriteLine($"Share exists: {share.Uri}");
+
+                // Get a reference to the sample directory
+                ShareDirectoryClient directory = share.GetDirectoryClient("CustomLogs");
+
+                if (await directory.ExistsAsync())
+                {
+                    Console.WriteLine($"Directory exists: {directory.Uri}");
+
+                    // Get a reference to a local file and upload it
+                    using (FileStream stream = File.OpenRead("downloadedLog1.txt"))
+                    {
+                        ShareFileClient file = await directory.CreateFileAsync("Log2.txt", stream.Length);
+                        await file.UploadAsync(stream);
+                        Console.WriteLine($"File uploaded: {file.Uri}");
+                    }
+                }
+            }
+            // </snippet_GenerateSAS>
+        }
+
+        // <snippet_CreateAcctSasUri>
+        //-------------------------------------------------
+        // Create a SAS URI for a storage account
+        //-------------------------------------------------
+        public Uri CreateAccountSasUri()
+        {
             // Get the account details from app settings
             string accountName = ConfigurationManager.AppSettings["StorageAccountName"];
             string accountKey = ConfigurationManager.AppSettings["StorageAccountKey"];
 
-            // Create a new shared access signature and define its constraints.
-            ShareSasBuilder shareSAS = new ShareSasBuilder()
+            AccountSasBuilder accountSAS = new AccountSasBuilder()
             {
-                // Access the logs share
-                ShareName = "logs",
-
-                // Specify an Azure Storage share resource
-                Resource = "s",
-
-                // Expires in 24 hours
-                ExpiresOn = DateTime.UtcNow.AddHours(24)
+                Services = (AccountSasServices.Blobs | AccountSasServices.Files),
+                ExpiresOn = DateTime.UtcNow.AddHours(24),
+                ResourceTypes = AccountSasResourceTypes.All
             };
 
-            // Allow read-write access
-            shareSAS.SetPermissions(ShareSasPermissions.Read | ShareSasPermissions.Write);
+            accountSAS.SetPermissions(AccountSasPermissions.Read | AccountSasPermissions.Write);
 
             // Create a SharedKeyCredential that we can use to sign the SAS token
             StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName, accountKey);
 
             // Build a SAS URI
-            UriBuilder sasUri = new UriBuilder($"https://{accountName}.file.core.windows.net");
-            sasUri.Query = shareSAS.ToSasQueryParameters(credential).ToString();
+            UriBuilder accountSasUri = new UriBuilder($"https://{accountName}.file.core.windows.net");
+            accountSasUri.Query = accountSAS.ToSasQueryParameters(credential).ToString();
 
-            // Create a ShareServiceClient which will be used to get a ShareClient
-            ShareServiceClient service = new ShareServiceClient(sasUri.Uri);
+            // Return the account SAS URI
+            return accountSasUri.Uri;
+        }
+        // </snippet_CreateAcctSasUri>
 
-            // Create a client that can authenticate with the SAS URI
-            ShareClient share = service.GetShareClient("logs");
-            Console.WriteLine($"Share client: {share.Name}");
+        //-------------------------------------------------
+        // Copy file programmatically
+        //-------------------------------------------------
+        public async Task CopyFileAsync()
+        {
+            // <snippet_CopyFile>
+            // Get the connection string from app settings
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
 
-            // Get a reference to the sample directory
-            ShareDirectoryClient directory = share.GetDirectoryClient("CustomLogs");
-            Console.WriteLine($"Directory client: {directory.Name}");
+            // Instantiate a ShareClient which will be used to access the file share
+            ShareClient share = new ShareClient(connectionString, "logs");
 
-            // Get a reference to a file and upload it
-            using (FileStream stream = File.OpenRead("downloadedLog1.txt"))
+            // Ensure that the share exists
+            if (await share.ExistsAsync())
             {
-                ShareFileClient file = await directory.CreateFileAsync("Log2.txt", stream.Length);
-                await file.UploadAsync(stream);
-                Console.WriteLine($"File uploaded: {file.Name}");
+                Console.WriteLine($"Share exists: {share.Uri}");
+
+                // Get a reference to the directory we created previously
+                ShareDirectoryClient directory = share.GetDirectoryClient("CustomLogs");
+
+                // Ensure that the directory exists
+                if (await directory.ExistsAsync())
+                {
+                    Console.WriteLine($"Directory exists: {directory.Uri}");
+
+                    // Get a reference to the file we created previously
+                    ShareFileClient sourceFile = directory.GetFileClient("Log1.txt");
+
+                    // Ensure that the source file exists
+                    if (await sourceFile.ExistsAsync())
+                    {
+                        // Get a reference to the destination file
+                        ShareFileClient destFile = directory.GetFileClient("Log1Copy.txt");
+
+                        // Start the copy operation
+                        await destFile.StartCopyAsync(sourceFile.Uri);
+
+                        if (await destFile.ExistsAsync())
+                        {
+                            Console.WriteLine($"{sourceFile.Uri} copied to {destFile.Uri}");
+                        }
+                    }
+                }
             }
-            // </snippet_GenerateSAS>
+            // </snippet_CopyFile>
         }
 
         //-------------------------------------------------
-        // Copy files programmatically
+        // Copy file programmatically
         //-------------------------------------------------
-        public async Task CopyFilesAsync()
+        public async Task CopyFileToBlobAsync()
         {
-            // <snippet_CopyFiles>
+            // <snippet_CopyFileToBlob>
+            Uri acctSasUri = CreateAccountSasUri();
 
-            // </snippet_CopyFiles>
+            // Create a ShareServiceClient which will be used to get a ShareClient
+            ShareServiceClient service = new ShareServiceClient(acctSasUri);
+
+            // Create a client that can authenticate with the SAS URI
+            ShareClient share = service.GetShareClient("logs");
+
+            // Ensure that the share exists
+            if (await share.ExistsAsync())
+            {
+                Console.WriteLine($"Share exists: {share.Uri}");
+
+                // Get a reference to the directory we created previously
+                ShareDirectoryClient directory = share.GetDirectoryClient("CustomLogs");
+
+                // Ensure that the directory exists
+                if (await directory.ExistsAsync())
+                {
+                    Console.WriteLine($"Directory exists: {directory.Uri}");
+
+                    // Get a reference to the file we created previously
+                    ShareFileClient sourceFile = directory.GetFileClient("Log1.txt");
+
+                    // Ensure that the source file exists
+                    if (await sourceFile.ExistsAsync())
+                    {
+                        // Get the connection string from app settings
+                        string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+                        // Get a reference to the destination container
+                        BlobContainerClient container = new BlobContainerClient(connectionString, "sample-container");
+
+                        // Create the container if it doesn't already exist
+                        await container.CreateIfNotExistsAsync();
+
+                        BlobClient destBlob = container.GetBlobClient("sample-blob.txt");
+
+                        await destBlob.StartCopyFromUriAsync(sourceFile.Uri);
+
+                        if (await destBlob.ExistsAsync())
+                        {
+                            Console.WriteLine($"File {sourceFile.Name} copied to blob {destBlob.Name}");
+                        }
+                    }
+                }
+            }
+            // </snippet_CopyFileToBlob>
         }
 
         //-------------------------------------------------
@@ -231,10 +342,11 @@ namespace dotnet_v12
             Console.WriteLine("1) Create file share client");
             Console.WriteLine("2) Access a file share programmatically");
             Console.WriteLine("3) Set the maximum size for a file share");
-            Console.WriteLine("4) Generate a SAS for a file or file share");
-            Console.WriteLine("5) Copy files");
-            Console.WriteLine("6) Share snapshots");
-            Console.WriteLine("7) Use metrics");
+            Console.WriteLine("4) Generate a SAS for an account");
+            Console.WriteLine("5) Copy a file");
+            Console.WriteLine("6) Copy a file to a blob");
+            Console.WriteLine("7) Share snapshots");
+            Console.WriteLine("8) Use metrics");
             Console.WriteLine("X) Exit to main menu");
             Console.Write("\r\nSelect an option: ");
 
@@ -268,22 +380,29 @@ namespace dotnet_v12
                     Console.ReadLine();
                     return true;
 
-                // Copy files
+                // Copy a file
                 case "5":
-                    await CopyFilesAsync();
+                    await CopyFileAsync();
+                    Console.WriteLine("Press enter to continue");
+                    Console.ReadLine();
+                    return true;
+
+                // Copy a file to a blob
+                case "6":
+                    await CopyFileToBlobAsync();
                     Console.WriteLine("Press enter to continue");
                     Console.ReadLine();
                     return true;
 
                 // Share snapshots
-                case "6":
+                case "7":
                     await ShareSnapshotsAsync();
                     Console.WriteLine("Press enter to continue");
                     Console.ReadLine();
                     return true;
 
                 // Use metrics
-                case "7":
+                case "8":
                     await UseMetricsAsync();
                     Console.WriteLine("Press enter to continue");
                     Console.ReadLine();
