7b7d48835e32803796c7b58cb3dfe67028ce9070
normesta <normesta@microsoft.com>
2020-06-12 15:40:46 -0700

    Adding page blob snippets

--- blobs/howto/dotnet/dotnet-v12/CRUD.cs
+++ blobs/howto/dotnet/dotnet-v12/CRUD.cs
@@ -24,14 +24,17 @@ using System.Linq;
 using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using Azure;
+using System.Text;
 
 namespace dotnet_v12
 {
     public class CRUD
     {
 
+        #region Blob flat listing
+
         //-------------------------------------------------
-        // Snippet1
+        // ListBlobsFlatListing
         //-------------------------------------------------
 
         // <Snippet_ListBlobsFlatListing>
@@ -74,7 +77,9 @@ namespace dotnet_v12
         }
         // </Snippet_ListBlobsFlatListing>
 
+        #endregion
 
+        #region Blob hierarchical listing
 
         //-------------------------------------------------
         // ListBlobsHierarchicalListing
@@ -134,21 +139,126 @@ namespace dotnet_v12
         }
         // </Snippet_ListBlobsHierarchicalListing>
 
+        #endregion
+
+        #region Create page blob
         //-------------------------------------------------
-        // CRUD menu (Can call asynchronous and synchronous methods)
+        // Create page blob
         //-------------------------------------------------
 
-        public async Task<bool> MenuAsync()
+        private static PageBlobClient CreatePageBlob(string connectionString)
+        {
+            // <Snippet_CreatePageBlob>
+            long OneGigabyteAsBytes = 1024 * 1024 * 1024;
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            var blobContainerClient =
+                blobServiceClient.GetBlobContainerClient(Constants.containerName);
+
+            var pageBlobClient = blobContainerClient.GetPageBlobClient("0s4.vhd");
+
+            pageBlobClient.Create(16 * OneGigabyteAsBytes);
+            // </Snippet_CreatePageBlob>
+
+            return pageBlobClient;
+        }
+
+        #endregion
+
+        #region Resize page blob
+
+        //-------------------------------------------------
+        // Resize page blob
+        //-------------------------------------------------
+
+        private static void ResizePageBlob(PageBlobClient pageBlobClient, 
+            long OneGigabyteAsBytes)
+        {
+            // <Snippet_ResizePageBlob>
+            pageBlobClient.Resize(32 * OneGigabyteAsBytes);
+            // </Snippet_ResizePageBlob>
+        }
+
+        #endregion
+
+        #region Write pages to a page blob
+
+        //-------------------------------------------------
+        // Write pages to a page blob
+        //-------------------------------------------------
+
+        private static void WriteToPageBlob(PageBlobClient pageBlobClient,
+            Stream dataStream)
+        {
+            long startingOffset = 512;
+
+            // <Snippet_WriteToPageBlob>
+            pageBlobClient.UploadPages(dataStream, startingOffset);
+            // </Snippet_WriteToPageBlob>
+        }
+
+        #endregion
+
+        #region Read pages from a page blob
+
+        //-------------------------------------------------
+        // Read pages from a page blob
+        //-------------------------------------------------
+
+        private static void ReadFromPageBlob(PageBlobClient pageBlobClient,
+            long bufferOffset, long rangeSize)
+        {
+            // <Snippet_ReadFromPageBlob>
+            var pageBlob = pageBlobClient.Download(new HttpRange(bufferOffset, rangeSize));
+            // </Snippet_ReadFromPageBlob>
+        }
+
+        #endregion
+
+        #region Read valid page regions from a page blob
+
+        //-------------------------------------------------
+        // Read valid page regions from a page blob
+        //-------------------------------------------------
+
+        private static void ReadValidPageRegionsFromPageBlob(PageBlobClient pageBlobClient,
+            long bufferOffset, long rangeSize)
+        {
+            // <Snippet_ReadValidPageRegionsFromPageBlob>
+            IEnumerable<HttpRange> pageRanges = pageBlobClient.GetPageRanges().Value.PageRanges;
+
+            foreach (var range in pageRanges)
+            {
+                var pageBlob = pageBlobClient.Download(range);
+            }
+             // </Snippet_ReadValidPageRegionsFromPageBlob>
+        }
+
+            #endregion
+
+        #region User menu
+
+            //-------------------------------------------------
+            // CRUD menu (Can call asynchronous and synchronous methods)
+            //-------------------------------------------------
+
+            public async Task<bool> MenuAsync()
         {
             Console.Clear();
             Console.WriteLine("Choose a Create, Read, Update, or Delete (CRUD) scenario:");
             Console.WriteLine("1) Show a flat listing of blobs");
             Console.WriteLine("2) Show a hierarchical listing of blobs");
-            Console.WriteLine("3) Return to main menu");
+            Console.WriteLine("3) Create a page blob");
+            Console.WriteLine("4) Resize a page blob");
+            Console.WriteLine("5) Write pages to a page blob");
+            Console.WriteLine("6) Read pages from a page blob");
+            Console.WriteLine("7) Read valid page regions from a page blob");
+            Console.WriteLine("8) Return to main menu");
             Console.Write("\r\nSelect an option: ");
 
-            var connectionString = Constants.connectionString;
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobServiceClient blobServiceClient = new BlobServiceClient(Constants.connectionString);
+            PageBlobClient pageBlobClient = null;
             
             switch (Console.ReadLine())
             {
@@ -168,6 +278,55 @@ namespace dotnet_v12
                     return true;
 
                 case "3":
+
+                    pageBlobClient = CreatePageBlob(Constants.connectionString);
+
+                    Console.ReadLine();
+                    return true;
+
+                case "4":
+
+                    if (pageBlobClient != null)
+                    {
+                        ResizePageBlob(pageBlobClient, 1024 * 1024 * 1024);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Need to create page blob first");
+                    }
+
+                    Console.ReadLine();
+                    return true;
+
+                case "5":
+
+                    UnicodeEncoding uniEncoding = new UnicodeEncoding();
+
+                    byte[] randomInformation = uniEncoding.GetBytes(
+                     "Random information");
+                    using (MemoryStream memStream = new MemoryStream(100))
+                    {
+                        memStream.Write(randomInformation, 0, randomInformation.Length);
+                        WriteToPageBlob(pageBlobClient, memStream);
+                    }
+                    Console.ReadLine();
+                    return true;
+
+                case "6":
+
+                    ReadFromPageBlob(pageBlobClient, 512, 200);
+
+                    Console.ReadLine();
+                    return true;
+
+                case "7":
+
+                    ReadValidPageRegionsFromPageBlob(pageBlobClient, 512, 200);
+
+                    Console.ReadLine();
+                    return true;
+
+                case "8":
                 
                    return false;
                 
@@ -176,11 +335,8 @@ namespace dotnet_v12
                    return true;
             }
         }
-        
+        #endregion
+
     }
 
-    
-
-
-    
 }
