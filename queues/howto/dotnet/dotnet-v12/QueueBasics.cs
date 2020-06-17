e29fc6d632a1f5c39746fe3d2d99688d2321ac53
AnuragSharma-MSFT <66360845+AnuragSharma-MSFT@users.noreply.github.com>
2020-06-17 11:26:55 +0530

    Fixing Code Snippet for GitHub Issue: 57033
    Fixing Code Snippet for GitHub Issue: 57033 - Updated the code as per comments

--- queues/howto/dotnet/dotnet-v12/QueueBasics.cs
+++ queues/howto/dotnet/dotnet-v12/QueueBasics.cs
@@ -88,8 +88,8 @@ namespace dotnet_v12
             // Instantiate a QueueClient which will be used to create and manipulate the queue
             QueueClient queueClient = new QueueClient(connectionString, "myqueue");
             
-             // Create the Base64 encoded UTF8 text message
-            string message = Convert.ToBase64String(Encoding.UTF8.GetBytes("On a dark desert highway"));
+             // Create the text message to add to the queue
+            string message = "First Message to azure Queue";
 
             if (queueClient.Exists())
             {
