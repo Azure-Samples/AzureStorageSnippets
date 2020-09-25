7ec66c916512b5f42271588d22a43d46017ec10c
Mark Hopkins <mhopkins@microsoft.com>
2020-09-25 16:51:46 -0700

    Created C++ quickstart

---
+++ blobs/quickstarts/C++/V12/BlobQuickstartV12/BlobQuickstartV12/BlobQuickstartV12.cpp
@@ -0,0 +1,95 @@
+// BlobQuickstartV12.cpp
+
+//<Snippet_Includes>
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <iostream>
+#include <stdlib.h>
+#include "azure\core\azure.hpp"
+#include "azure\storage\common\storage_common.hpp"
+#include "azure\storage\blobs\blob.hpp"
+//</Snippet_Includes>
+
+int main()
+{
+    std::cout << "Azure Blob storage v12 - C++ quickstart sample" << std::endl;
+
+    //<Snippet_ConnectionString>
+    // Retrieve the connection string for use with the application. The storage
+    // connection string is stored in an environment variable on the machine
+    // running the application called AZURE_STORAGE_CONNECTION_STRING. If the
+    // environment variable is created after the application is launched in a
+    // console or with Visual Studio, the shell or application needs to be closed
+    // and reloaded to take the environment variable into account.
+    const char* connectionString = std::getenv("AZURE_STORAGE_CONNECTION_STRING");
+    //</Snippet_ConnectionString>
+
+    using namespace Azure::Storage::Blobs;
+
+    std::string containerName = "myblobcontainer";
+    std::string blobName = "blob.txt";
+    std::string blobContent = "Hello Azure!";
+
+    try
+    {
+        //<Snippet_CreateContainer>
+        // Initialize a new instance of BlobContainerClient
+        BlobContainerClient containerClient =
+            BlobContainerClient::CreateFromConnectionString(connectionString, containerName);
+
+        try
+        {
+            // Create the container. This will throw an exception if the container already exists.
+            containerClient.Create();
+        }
+        catch (std::runtime_error& e)
+        {
+            std::cout << "Error: " << e.what() << std::endl;
+        }
+        //</Snippet_CreateContainer>
+
+        //<Snippet_UploadBlob>
+        // Create the block blob client
+        BlockBlobClient blobClient = containerClient.GetBlockBlobClient(blobName);
+
+        // Upload the blob
+        std::cout << "Uploading blob: " << blobName << std::endl;
+        blobClient.UploadFrom(reinterpret_cast<const uint8_t*>(blobContent.data()), blobContent.size());
+        //</Snippet_UploadBlob>
+
+        //<Snippet_ListBlobs>
+        std::cout << "Listing blobs..." << std::endl;
+
+        auto response = containerClient.ListBlobsFlatSegment();
+
+        ListBlobsFlatSegmentResult result = response.ExtractValue();
+
+        for (BlobItem blobItem : result.Items)
+        {
+            std::cout << "Blob name: " << blobItem.Name.data() << std::endl;
+        }
+        //</Snippet_ListBlobs>
+
+        //<Snippet_DownloadBlob>
+        blobClient.DownloadTo(reinterpret_cast<uint8_t*>(&blobContent[0]), blobContent.size());
+
+        std::cout << blobContent << std::endl;
+        //</Snippet_DownloadBlob>
+
+        //<Snippet_DeleteBlob>
+        blobClient.Delete();
+        //</Snippet_DeleteBlob>
+
+        //<Snippet_DeleteContainer>
+        containerClient.Delete();
+        //</Snippet_DeleteContainer>
+
+    }
+    catch (std::runtime_error& e)
+    {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    
+}
