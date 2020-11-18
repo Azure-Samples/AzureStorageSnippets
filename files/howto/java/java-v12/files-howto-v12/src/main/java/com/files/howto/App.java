94f968fc0f51d46b404d21bd3084acf3b148f4a2
Mark Hopkins <mhopkins@microsoft.com>
2020-11-18 13:43:01 -0800

    Code complete

--- files/howto/java/java-v12/files-howto-v12/src/main/java/com/files/howto/App.java
+++ files/howto/java/java-v12/files-howto-v12/src/main/java/com/files/howto/App.java
@@ -1,17 +1,14 @@
 package com.files.howto;
 
-// import java.time.*;
-// import java.time.Duration;
+//import java.time.Duration.*;
 //import java.util.UUID.*;
 
 /**
- * Azure file storage v12 SDK how-to
- */
-
-// import com.azure.core.*;
+* Azure file storage v12 SDK how-to
+*/
 
 // <Snippet_ImportStatements>
-// Include the following imports to use queue APIs
+// Include the following imports to use Azure Files APIs
 import com.azure.storage.file.share.*;
 // </Snippet_ImportStatements>
 
@@ -19,28 +16,26 @@ public class App
 {
     public static void main( String[] args )
     {
+        String fileName = "Log1.txt";
+
         // <Snippet_ConnectionString>
-        // Define the connection-string with your values
+        // Define the connection-string.
+        // Replace the values, including <>, with
+        // the values from your storage account.
         final String connectStr = 
-            "DefaultEndpointsProtocol=https;" +
-            "AccountName=<storage_account_name>;" +
-            "AccountKey=<storage_account_key>";
+           "DefaultEndpointsProtocol=https;" +
+           "AccountName=<storage_account_name>;" +
+           "AccountKey=<storage_account_key>";
         // </Snippet_ConnectionString>
 
-        String shareName = createFileShare(connectStr);
-
-        String dirName = createDirectory(connectStr, shareName);
-
-        String fileName = "Log1.txt";
-        uploadFile(connectStr, shareName, fileName);
-
-        // enumerateFilesAndDirs
-        // downloadFile
-        // deleteFile
-
-        deleteDirectory(connectStr, shareName, dirName);
-
-        deleteFileShare(connectStr, shareName);
+        String shareName = createFileShare(connectStr);             // Create a new file share
+        String dirName = createDirectory(connectStr, shareName);    // Create a directory in the new file share
+        uploadFile(connectStr, shareName, "", fileName);            // Upload a file to the file share root directory
+        enumerateFilesAndDirs(connectStr, shareName, "");           // Enumerate the file share root directory
+        downloadFile(connectStr, shareName, "", fileName);          // Download a file from the file share root directory
+        deleteFile(connectStr, shareName, "", fileName);            // Delete a file from the file share root directory
+        deleteDirectory(connectStr, shareName, dirName);            // Delete the specified directory
+        deleteFileShare(connectStr, shareName);                     // Delete the file share
     }
 
     // <Snippet_createFileShare>
@@ -61,7 +56,6 @@ public class App
         {
             // Output the exception message and stack trace
             System.out.println("Exception: " + e.getMessage());
-            e.printStackTrace();
             return null;
         }
     }
@@ -85,14 +79,13 @@ public class App
         {
             // Output the exception message and stack trace
             System.out.println("Exception: " + e.getMessage());
-            e.printStackTrace();
             return null;
         }
     }
     // </Snippet_createDirectory>
 
     // <Snippet_uploadFile>
-    public static Boolean uploadFile(String connectStr, String shareName, String fileName)
+    public static Boolean uploadFile(String connectStr, String shareName, String dirName, String fileName)
     {
         try
         {
@@ -100,9 +93,10 @@ public class App
                 .connectionString(connectStr).shareName(shareName)
                 .buildClient();
 
-            ShareDirectoryClient rootDirClient = shareClient.getRootDirectoryClient();
+            ShareDirectoryClient dirClient = shareClient.getDirectoryClient(dirName);
 
-            ShareFileClient fileClient = rootDirClient.getFileClient(fileName);
+            ShareFileClient fileClient = dirClient.getFileClient(fileName);
+            fileClient.create(1024);
             fileClient.uploadFromFile(fileName);
             return true;
         }
@@ -110,17 +104,85 @@ public class App
         {
             // Output the exception message and stack trace
             System.out.println("Exception: " + e.getMessage());
-            e.printStackTrace();
             return false;
         }
     }
     // </Snippet_uploadFile>
 
-    // public static Boolean enumerateFilesAndDirs(String connectStr, String shareName, String dirName)
+    // <Snippet_enumerateFilesAndDirs>
+    public static Boolean enumerateFilesAndDirs(String connectStr, String shareName, String dirName)
+    {
+        try
+        {
+            ShareClient shareClient = new ShareClientBuilder()
+                .connectionString(connectStr).shareName(shareName)
+                .buildClient();
 
-    // public static Boolean downloadFile(String connectStr, String shareName, String fileName)
+            ShareDirectoryClient dirClient = shareClient.getDirectoryClient(dirName);
 
-    // public static Boolean deleteFile(String connectStr, String shareName, String fileName)
+            dirClient.listFilesAndDirectories().forEach(
+                fileRef -> System.out.printf("Directory? %b. Resource name: %s.\n",
+                fileRef.isDirectory(), fileRef.getName())
+            );
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            // Output the exception message and stack trace
+            System.out.println("Exception: " + e.getMessage());
+            return false;
+        }
+    }
+    // </Snippet_enumerateFilesAndDirs>
+
+    // <Snippet_downloadFile>
+    public static Boolean downloadFile(String connectStr, String shareName, String dirName, String fileName)
+    {
+        try
+        {
+            ShareClient shareClient = new ShareClientBuilder()
+                .connectionString(connectStr).shareName(shareName)
+                .buildClient();
+
+            ShareDirectoryClient dirClient = shareClient.getDirectoryClient(dirName);
+
+            ShareFileClient fileClient = dirClient.getFileClient(fileName);
+            fileClient.downloadToFile("DOWNLOADED_" + fileName);
+            return true;
+        }
+        catch (Exception e)
+        {
+            // Output the exception message and stack trace
+            System.out.println("Exception: " + e.getMessage());
+            return false;
+        }
+    }
+    // </Snippet_downloadFile>
+
+    // <Snippet_deleteFile>
+    public static Boolean deleteFile(String connectStr, String shareName, String dirName, String fileName)
+    {
+        try
+        {
+            ShareClient shareClient = new ShareClientBuilder()
+                .connectionString(connectStr).shareName(shareName)
+                .buildClient();
+
+            ShareDirectoryClient dirClient = shareClient.getDirectoryClient(dirName);
+
+            ShareFileClient fileClient = dirClient.getFileClient(fileName);
+            fileClient.delete();
+            return true;
+        }
+        catch (Exception e)
+        {
+            // Output the exception message and stack trace
+            System.out.println("Exception: " + e.getMessage());
+            return false;
+        }
+    }
+    // </Snippet_deleteFile>
 
     // <Snippet_deleteDirectory>
     public static Boolean deleteDirectory(String connectStr, String shareName, String dirName)
@@ -139,7 +201,6 @@ public class App
         {
             // Output the exception message and stack trace
             System.out.println("Exception: " + e.getMessage());
-            e.printStackTrace();
             return false;
         }
     }
@@ -161,7 +222,6 @@ public class App
         {
             // Output the exception message and stack trace
             System.out.println("Exception: " + e.getMessage());
-            e.printStackTrace();
             return false;
         }
     }
