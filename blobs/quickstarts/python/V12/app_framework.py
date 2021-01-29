39d9cfb0d33e9ec01bc6aca9939e66c4ef5e4fde
Mark Hopkins <mhopkins@microsoft.com>
2021-01-28 16:51:58 -0800

    Initial commit

---
+++ blobs/quickstarts/python/V12/app_framework.py
@@ -0,0 +1,11 @@
+import os, uuid
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+
+try:
+    print("Azure Blob Storage v" + __version__ + " - Python quickstart sample")
+
+    # Quick start code goes here
+
+except Exception as ex:
+    print('Exception:')
+    print(ex)
---
+++ blobs/quickstarts/python/V12/blob-quickstart-v12.py
@@ -0,0 +1,105 @@
+#----------------------------------------------------------------------------------
+# Microsoft Developer & Platform Evangelism
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+# OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#----------------------------------------------------------------------------------
+# The example companies, organizations, products, domain names,
+# e-mail addresses, logos, people, places, and events depicted
+# herein are fictitious.  No association with any real company,
+# organization, product, domain name, email address, logo, person,
+# places, or events is intended or should be inferred.
+#----------------------------------------------------------------------------------
+
+import os, uuid
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+
+try:
+    print("Azure Blob Storage v" + __version__ + " - Python quickstart sample")
+
+    #<Snippet_ConnectionString>
+    # Retrieve the connection string for use with the application. The storage
+    # connection string is stored in an environment variable on the machine
+    # running the application called AZURE_STORAGE_CONNECTION_STRING. If the environment variable is
+    # created after the application is launched in a console or with Visual Studio,
+    # the shell or application needs to be closed and reloaded to take the
+    # environment variable into account.
+    connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+    #</Snippet_ConnectionString>
+
+    #<Snippet_CreateContainer>
+    # Create the BlobServiceClient object which will be used to create a container client
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+    # Create a unique name for the container
+    container_name = str(uuid.uuid4())
+
+    # Create the container
+    container_client = blob_service_client.create_container(container_name)
+    #</Snippet_CreateContainer>
+
+    #<Snippet_UploadBlobs>
+    # Create a local directory to hold blob data
+    local_path = "./data"
+    os.mkdir(local_path)
+
+    # Create a file in the local data directory to upload and download
+    local_file_name = str(uuid.uuid4()) + ".txt"
+    upload_file_path = os.path.join(local_path, local_file_name)
+
+    # Write text to the file
+    file = open(upload_file_path, 'w')
+    file.write("Hello, World!")
+    file.close()
+
+    # Create a blob client using the local file name as the name for the blob
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+
+    print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+
+    # Upload the created file
+    with open(upload_file_path, "rb") as data:
+        blob_client.upload_blob(data)
+    #</Snippet_UploadBlobs>
+
+    #<Snippet_ListBlobs>
+    print("\nListing blobs...")
+
+    # List the blobs in the container
+    blob_list = container_client.list_blobs()
+    for blob in blob_list:
+        print("\t" + blob.name)
+    #</Snippet_ListBlobs>
+
+    #<Snippet_DownloadBlob>
+    # Download the blob to a local file
+    # Add 'DOWNLOAD' before the .txt extension so you can see both files in the data directory
+    download_file_path = os.path.join(local_path, str.replace(local_file_name ,'.txt', 'DOWNLOAD.txt'))
+    print("\nDownloading blob to \n\t" + download_file_path)
+
+    with open(download_file_path, "wb") as download_file:
+        download_file.write(blob_client.download_blob().readall())
+    #</Snippet_DownloadBlob>
+
+    #<Snippet_DeleteContainer>
+    # Clean up
+    print("\nPress the Enter key to begin clean up")
+    input()
+
+    print("Deleting blob container...")
+    container_client.delete_container()
+
+    print("Deleting the local source and downloaded files...")
+    os.remove(upload_file_path)
+    os.remove(download_file_path)
+    os.rmdir(local_path)
+
+    print("Done")
+    #</Snippet_DeleteContainer>
+
+except Exception as ex:
+    print('Exception:')
+    print(ex)
