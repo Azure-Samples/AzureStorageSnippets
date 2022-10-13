5f896d90c3d738e683a12c2cf385e4d14e834e8a
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2022-10-13 14:07:01 -0400

    Fix snippet

--- blobs/quickstarts/python/V12/blob-quickstart.py
+++ blobs/quickstarts/python/V12/blob-quickstart.py
@@ -21,6 +21,16 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 try:
     print("Azure Blob Storage Python quickstart sample")
 
+    #<Snippet_ConnectionString>
+    # Retrieve the connection string for use with the application. The storage
+    # connection string is stored in an environment variable on the machine
+    # running the application called AZURE_STORAGE_CONNECTION_STRING. If the environment variable is
+    # created after the application is launched in a console or with Visual Studio,
+    # the shell or application needs to be closed and reloaded to take the
+    # environment variable into account.
+    connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+    #</Snippet_ConnectionString>
+
     #<Snippet_CreateServiceClientDAC>
     account_url = "https://<storageaccountname>.blob.core.windows.net"
     default_credential = DefaultAzureCredential()
