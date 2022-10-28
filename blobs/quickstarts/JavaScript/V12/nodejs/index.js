989b5e176dc92efc23c75c5917f4b411c0974f25
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-10-28 08:41:49 -0700

    format

--- blobs/quickstarts/JavaScript/V12/nodejs/index.js
+++ blobs/quickstarts/JavaScript/V12/nodejs/index.js
@@ -1,8 +1,8 @@
 // <snippet_ImportLibrary>
 // index.js
-const { BlobServiceClient } = require("@azure/storage-blob");
-const { v1: uuidv1 } = require("uuid");
-require("dotenv").config();
+const { BlobServiceClient } = require('@azure/storage-blob');
+const { v1: uuidv1 } = require('uuid');
+require('dotenv').config();
 // </snippet_ImportLibrary>
 
 async function main() {
@@ -12,7 +12,7 @@ async function main() {
       process.env.AZURE_STORAGE_CONNECTION_STRING;
 
     if (!AZURE_STORAGE_CONNECTION_STRING) {
-      throw Error("Azure Storage Connection string not found");
+      throw Error('Azure Storage Connection string not found');
 
     // Create the BlobServiceClient object with connection string
     const blobServiceClient = BlobServiceClient.fromConnectionString(
@@ -22,12 +22,12 @@ async function main() {
     // </snippet_StorageAcctInfo__with_secrets>
 
     // <snippet_StorageAcctInfo_without_secrets>
-    const { DefaultAzureCredential } = require("@azure/identity");
+    const { DefaultAzureCredential } = require('@azure/identity');
     // </snippet_StorageAcctInfo_without_secrets>
 
     // <snippet_StorageAcctInfo_create_client>
     const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
-    if (!accountName) throw Error("Azure Storage accountName not found");
+    if (!accountName) throw Error('Azure Storage accountName not found');
 
     const blobServiceClient = new BlobServiceClient(
       `https://${accountName}.blob.core.windows.net`,
@@ -36,14 +36,14 @@ async function main() {
     // <snippet_StorageAcctInfo_create_client>
     
 
-    console.log("Azure Blob storage v12 - JavaScript quickstart sample");
+    console.log('Azure Blob storage v12 - JavaScript quickstart sample');
 
     // <snippet_CreateContainer>
     // Create a unique name for the container
-    const containerName = "quickstart" + uuidv1();
+    const containerName = 'quickstart' + uuidv1();
 
-    console.log("\nCreating container...");
-    console.log("\t", containerName);
+    console.log('\nCreating container...');
+    console.log('\t', containerName);
 
     // Get a reference to a container
     const containerClient = blobServiceClient.getContainerClient(containerName);
@@ -56,7 +56,7 @@ async function main() {
 
     // <snippet_UploadBlobs>
     // Create a unique name for the blob
-    const blobName = "quickstart" + uuidv1() + ".txt";
+    const blobName = 'quickstart' + uuidv1() + '.txt';
 
     // Get a block blob client
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
@@ -67,7 +67,7 @@ async function main() {
     );
 
     // Upload data to the blob
-    const data = "Hello, World!";
+    const data = 'Hello, World!';
     const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
     console.log(
       `Blob was uploaded successfully. requestId: ${uploadBlobResponse.requestId}`
@@ -75,7 +75,7 @@ async function main() {
     // </snippet_UploadBlobs>
 
     // <snippet_ListBlobs>
-    console.log("\nListing blobs...");
+    console.log('\nListing blobs...');
 
     // List the blob(s) in the container.
     for await (const blob of containerClient.listBlobsFlat()) {
@@ -94,20 +94,20 @@ async function main() {
     // In Node.js, get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
     // In browsers, get downloaded data by accessing downloadBlockBlobResponse.blobBody
     const downloadBlockBlobResponse = await blockBlobClient.download(0);
-    console.log("\nDownloaded blob content...");
+    console.log('\nDownloaded blob content...');
     console.log(
-      "\t",
+      '\t',
       await streamToText(downloadBlockBlobResponse.readableStreamBody)
     );
     // </snippet_DownloadBlobs>
 
     // <snippet_DeleteContainer>
     // Delete container
-    console.log("\nDeleting container...");
+    console.log('\nDeleting container...');
 
     const deleteContainerResponse = await containerClient.delete();
     console.log(
-      "Container was deleted successfully. requestId: ",
+      'Container was deleted successfully. requestId: ',
       deleteContainerResponse.requestId
     );
     // </snippet_DeleteContainer>
@@ -119,8 +119,8 @@ async function main() {
 // <snippet_ConvertStreamToText>
 // Convert stream to text
 async function streamToText(readable) {
-  readable.setEncoding("utf8");
-  let data = "";
+  readable.setEncoding('utf8');
+  let data = '';
   for await (const chunk of readable) {
     data += chunk;
   }
@@ -130,6 +130,6 @@ async function streamToText(readable) {
 
 // <snippet_CallMain>
 main()
-  .then(() => console.log("Done"))
+  .then(() => console.log('Done'))
   .catch((ex) => console.log(ex.message));
 // </snippet_CallMain>
