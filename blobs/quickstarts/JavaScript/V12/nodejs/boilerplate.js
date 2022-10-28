0629b136277dde7c14cc9aa1654637af9a45d1f7
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-10-28 08:34:33 -0700

    updates for passwordless

--- blobs/quickstarts/JavaScript/V12/nodejs/boilerplate.js
+++ blobs/quickstarts/JavaScript/V12/nodejs/boilerplate.js
@@ -1,14 +1,20 @@
-const { BlobServiceClient } = require('@azure/storage-blob');
-    const { v1: uuidv1} = require('uuid');
-    require('dotenv').config()
-    
-    async function main() {
-        console.log('Azure Blob storage v12 - JavaScript quickstart sample');
+const { BlobServiceClient } = require("@azure/storage-blob");
+const { v1: uuidv1 } = require("uuid");
+require("dotenv").config();
 
-        // Quick start code goes here
+async function main() {
+  try {
+    console.log("Azure Blob storage v12 - JavaScript quickstart sample");
 
+    // Quick start code goes here
+
+
+
+  } catch (err) {
+    console.err(`Error: ${err.message}`);
+  }
 }
 
 main()
-.then(() => console.log('Done'))
-.catch((ex) => console.log(ex.message));
+  .then(() => console.log("Done"))
+  .catch((ex) => console.log(ex.message));
--- blobs/quickstarts/JavaScript/V12/nodejs/index.js
+++ blobs/quickstarts/JavaScript/V12/nodejs/index.js
@@ -2,114 +2,125 @@
 // index.js
 const { BlobServiceClient } = require("@azure/storage-blob");
 const { v1: uuidv1 } = require("uuid");
-require('dotenv').config()
+require("dotenv").config();
 // </snippet_ImportLibrary>
 
-// <snippet_StorageAcctInfo__with_secrets>
-/* const AZURE_STORAGE_CONNECTION_STRING = 
-  process.env.AZURE_STORAGE_CONNECTION_STRING;
-
-if (!AZURE_STORAGE_CONNECTION_STRING) {
-  throw Error("Azure Storage Connection string not found");
-
-// Create the BlobServiceClient object with connection string
-const blobServiceClient = BlobServiceClient.fromConnectionString(
-  AZURE_STORAGE_CONNECTION_STRING
-);
-
-*/
-// </snippet_StorageAcctInfo__with_secrets>
-
-// <snippet_StorageAcctInfo_without_secrets>
-// Connect without secrets to Azure
-// Learn more: https://www.npmjs.com/package/@azure/identity#DefaultAzureCredential
-const { DefaultAzureCredential } = require('@azure/identity');
-const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
-if (!accountName) throw Error('Azure Storage accountName not found');
-
-const blobServiceClient = new BlobServiceClient(
-  `https://${accountName}.blob.core.windows.net`,
-  new DefaultAzureCredential()
-);
-// </snippet_StorageAcctInfo_without_secrets>
-
-
 async function main() {
-  console.log("Azure Blob storage v12 - JavaScript quickstart sample");
+  try {
+    // <snippet_StorageAcctInfo__with_secrets>
+    /* const AZURE_STORAGE_CONNECTION_STRING = 
+      process.env.AZURE_STORAGE_CONNECTION_STRING;
 
-  // <snippet_CreateContainer>
-  // Create a unique name for the container
-  const containerName = "quickstart" + uuidv1();
+    if (!AZURE_STORAGE_CONNECTION_STRING) {
+      throw Error("Azure Storage Connection string not found");
 
-  console.log("\nCreating container...");
-  console.log("\t", containerName);
+    // Create the BlobServiceClient object with connection string
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+      AZURE_STORAGE_CONNECTION_STRING
+    );
+    */
+    // </snippet_StorageAcctInfo__with_secrets>
 
-  // Get a reference to a container
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-  // Create the container
-  const createContainerResponse = await containerClient.create();
-  console.log(`Container was created successfully.\n\trequestId:${createContainerResponse.requestId}\n\tURL: ${containerClient.url}`);
-  // </snippet_CreateContainer>
+    // <snippet_StorageAcctInfo_without_secrets>
+    const { DefaultAzureCredential } = require("@azure/identity");
+    // </snippet_StorageAcctInfo_without_secrets>
 
-  // <snippet_UploadBlobs>
-  // Create a unique name for the blob
-  const blobName = "quickstart" + uuidv1() + ".txt";
+    // <snippet_StorageAcctInfo_create_client>
+    const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+    if (!accountName) throw Error("Azure Storage accountName not found");
 
-  // Get a block blob client
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const blobServiceClient = new BlobServiceClient(
+      `https://${accountName}.blob.core.windows.net`,
+      new DefaultAzureCredential()
+    );
+    // <snippet_StorageAcctInfo_create_client>
+    
 
-  // Display blob name and url
-  console.log(`\nUploading to Azure storage as blob\n\tname: ${blobName}:\n\tURL: ${blockBlobClient.url}`);
+    console.log("Azure Blob storage v12 - JavaScript quickstart sample");
 
-  // Upload data to the blob
-  const data = "Hello, World!";
-  const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
-  console.log(`Blob was uploaded successfully. requestId: ${uploadBlobResponse.requestId}`);
-  // </snippet_UploadBlobs>
+    // <snippet_CreateContainer>
+    // Create a unique name for the container
+    const containerName = "quickstart" + uuidv1();
 
-  // <snippet_ListBlobs>
-  console.log("\nListing blobs...");
+    console.log("\nCreating container...");
+    console.log("\t", containerName);
 
-  // List the blob(s) in the container.
-  for await (const blob of containerClient.listBlobsFlat()) {
+    // Get a reference to a container
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    // Create the container
+    const createContainerResponse = await containerClient.create();
+    console.log(
+      `Container was created successfully.\n\trequestId:${createContainerResponse.requestId}\n\tURL: ${containerClient.url}`
+    );
+    // </snippet_CreateContainer>
 
-    // Get Blob Client from name, to get the URL
-    const tempBlockBlobClient = containerClient.getBlockBlobClient(blob.name);
+    // <snippet_UploadBlobs>
+    // Create a unique name for the blob
+    const blobName = "quickstart" + uuidv1() + ".txt";
 
-    // Display blob name and URL
-    console.log(`\n\tname: ${blob.name}\n\tURL: ${tempBlockBlobClient.url}\n`);
+    // Get a block blob client
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    // Display blob name and url
+    console.log(
+      `\nUploading to Azure storage as blob\n\tname: ${blobName}:\n\tURL: ${blockBlobClient.url}`
+    );
+
+    // Upload data to the blob
+    const data = "Hello, World!";
+    const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+    console.log(
+      `Blob was uploaded successfully. requestId: ${uploadBlobResponse.requestId}`
+    );
+    // </snippet_UploadBlobs>
+
+    // <snippet_ListBlobs>
+    console.log("\nListing blobs...");
+
+    // List the blob(s) in the container.
+    for await (const blob of containerClient.listBlobsFlat()) {
+      // Get Blob Client from name, to get the URL
+      const tempBlockBlobClient = containerClient.getBlockBlobClient(blob.name);
+
+      // Display blob name and URL
+      console.log(
+        `\n\tname: ${blob.name}\n\tURL: ${tempBlockBlobClient.url}\n`
+      );
+    }
+    // </snippet_ListBlobs>
+
+    // <snippet_DownloadBlobs>
+    // Get blob content from position 0 to the end
+    // In Node.js, get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
+    // In browsers, get downloaded data by accessing downloadBlockBlobResponse.blobBody
+    const downloadBlockBlobResponse = await blockBlobClient.download(0);
+    console.log("\nDownloaded blob content...");
+    console.log(
+      "\t",
+      await streamToText(downloadBlockBlobResponse.readableStreamBody)
+    );
+    // </snippet_DownloadBlobs>
+
+    // <snippet_DeleteContainer>
+    // Delete container
+    console.log("\nDeleting container...");
+
+    const deleteContainerResponse = await containerClient.delete();
+    console.log(
+      "Container was deleted successfully. requestId: ",
+      deleteContainerResponse.requestId
+    );
+    // </snippet_DeleteContainer>
+
+  } catch (err) {
+    console.err(`Error: ${err.message}`);
   }
-  // </snippet_ListBlobs>
-
-  // <snippet_DownloadBlobs>
-  // Get blob content from position 0 to the end
-  // In Node.js, get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
-  // In browsers, get downloaded data by accessing downloadBlockBlobResponse.blobBody
-  const downloadBlockBlobResponse = await blockBlobClient.download(0);
-  console.log("\nDownloaded blob content...");
-  console.log(
-    "\t",
-    await streamToText(downloadBlockBlobResponse.readableStreamBody)
-  );
-  // </snippet_DownloadBlobs>
-
-  // <snippet_DeleteContainer>
-  // Delete container
-  console.log("\nDeleting container...");
-
-  const deleteContainerResponse = await containerClient.delete();
-  console.log(
-    "Container was deleted successfully. requestId: ",
-    deleteContainerResponse.requestId
-  );
-  // </snippet_DeleteContainer>
-
 }
 // <snippet_ConvertStreamToText>
 // Convert stream to text
 async function streamToText(readable) {
-  readable.setEncoding('utf8');
-  let data = '';
+  readable.setEncoding("utf8");
+  let data = "";
   for await (const chunk of readable) {
     data += chunk;
   }
