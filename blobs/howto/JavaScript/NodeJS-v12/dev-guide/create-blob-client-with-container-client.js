400967a3dadd6ded9f66e1dcd18633f0e35d8b2d
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-11-30 09:21:12 -0800

    JS Dev Guide - create client from parent client

---
+++ blobs/howto/JavaScript/NodeJS-v12/dev-guide/create-blob-client-with-container-client.js
@@ -0,0 +1,57 @@
+// Azure Storage dependency
+const {
+  StorageSharedKeyCredential,
+  ContainerClient
+} = require("@azure/storage-blob");
+
+// For development environment - include environment variables from .env
+require("dotenv").config();
+
+// Azure Storage resource name
+const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+if (!accountName) throw Error("Azure Storage accountName not found");
+
+// Azure Storage resource key
+const accountKey = process.env.AZURE_STORAGE_ACCOUNT_KEY;
+if (!accountKey) throw Error("Azure Storage accountKey not found");
+
+// Create credential
+const sharedKeyCredential = new StorageSharedKeyCredential(
+  accountName,
+  accountKey
+);
+
+const baseUrl = `https://${accountName}.blob.core.windows.net`;
+const containerName = `my-container`;
+const blobName = `my-blob`;
+
+// Create ContainerClient
+const containerClient = new ContainerClient(
+  `${baseUrl}/${containerName}`,
+  sharedKeyCredential
+);  
+
+async function main() {
+  try {
+  
+    const blobClient = containerClient.getBlobClient(blobName);
+
+    // do something with blobClient...
+    const properties = await blobClient.getProperties();
+    console.log(`Blob ${blobName} properties:`);
+
+    // get BlockBlobClient from blobClient
+    const blockBlobClient = blobClient.getBlockBlobClient();
+
+    // do something with blockBlobClient...
+    const downloadResponse = await blockBlobClient.download(0);
+
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+main()
+  .then(() => console.log(`done`))
+  .catch((ex) => console.log(ex.message));
