18b70233141bc4deeda84de6a28421ecca3fd004
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2022-12-12 16:42:22 -0500

    Update lease code samples

--- blobs/howto/Java/blob-devguide/blob-devguide-containers/src/main/java/com/blobs/devguide/containers/App.java
+++ blobs/howto/Java/blob-devguide/blob-devguide-containers/src/main/java/com/blobs/devguide/containers/App.java
@@ -2,6 +2,7 @@ package com.blobs.devguide.containers;
 
 import com.azure.identity.*;
 import com.azure.storage.blob.*;
+import com.azure.storage.blob.specialized.BlobLeaseClient;
 
 public class App {
     public static void main(String[] args) {
@@ -18,7 +19,7 @@ public class App {
         // Azure SDK client builders accept the credential as a parameter
         // TODO: Replace <storage-account-name> with your actual storage account name
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-                .endpoint("https://<storage-account-name>.blob.core.windows.net/")
+                .endpoint("https://pjstorageaccounttest.blob.core.windows.net/")
                 .credential(defaultCredential)
                 .buildClient();
 
@@ -35,6 +36,10 @@ public class App {
         ContainerList listHelper = new ContainerList();
         listHelper.listContainers(blobServiceClient);
         listHelper.listContainersWithPaging(blobServiceClient);
+
+        ContainerLease leaseHelper = new ContainerLease();
+        BlobLeaseClient leaseClient =  leaseHelper.acquireContainerLease(blobContainerClient);
+        leaseHelper.releaseContainerLease(leaseClient);
 
         ContainerDelete deleteHelper = new ContainerDelete();
         deleteHelper.deleteContainer(blobServiceClient, containerName);
