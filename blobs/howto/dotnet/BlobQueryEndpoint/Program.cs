6879374479eea62eebb6162ed828d13a0d7bb2e3
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2023-03-03 15:02:41 -0500

    Add sample to query endpoints using SRP

---
+++ blobs/howto/dotnet/BlobQueryEndpoint/Program.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+using ManagementTasks;
+
+// Create an instance of DefaultAzureCredential for authorization
+TokenCredential credential = new DefaultAzureCredential();
+
+// TODO: replace with your storage account name
+string storageAccountName = "<storage-account-name>";
+
+string endpoint = await AccountProperties.GetBlobServiceEndpoint(storageAccountName, credential);
+Console.WriteLine($"URI: {endpoint}");
+
+// Now that we know the endpoint, create the client object
+BlobServiceClient blobServiceClient = new(new Uri(endpoint), credential);
+
+// List containers in the storage account
+Console.WriteLine("Listing containers ...");
+
+await foreach (BlobContainerItem container in blobServiceClient.GetBlobContainersAsync())
+{
+    Console.WriteLine("\t" + container.Name);
+}
---
+++ blobs/howto/dotnet/BlobQueryEndpoint/QueryEndpoint.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Core;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Storage;
+
+namespace ManagementTasks
+{
+    public static class AccountProperties
+    {
+        public static async Task<string> GetBlobServiceEndpoint(string storageAccountName, TokenCredential credential)
+        {
+            // You can locate your subscription ID on the Subscriptions blade of the Azure Portal (https://portal.azure.com)
+            const string subscriptionId = "<subscription-id>";
+
+            // TODO: replace your resource group name
+            const string rgName = "<resource-group-name>";
+
+            ArmClient armClient = new(credential);
+
+            // Create a resource identifier, then get the subscription resource
+            ResourceIdentifier resourceIdentifier = new($"/subscriptions/{subscriptionId}");
+            SubscriptionResource subscription = armClient.GetSubscriptionResource(resourceIdentifier);
+
+            // Register the Storage resource provider in the subscription
+            ResourceProviderResource resourceProvider = await subscription.GetResourceProviderAsync("Microsoft.Storage");
+            resourceProvider.Register();
+
+            // Get a resource group
+            ResourceGroupResource resourceGroup = await subscription.GetResourceGroupAsync(rgName);
+
+            // Get a collection of storage account resources
+            StorageAccountCollection accountCollection = resourceGroup.GetStorageAccounts();
+
+            // Get the properties for the specified storage account
+            StorageAccountResource storageAccount = await accountCollection.GetAsync(storageAccountName);
+
+            // Get the primary endpoint for the blob service
+            string endpoint = storageAccount.Data.PrimaryEndpoints.BlobUri.ToString();
+
+            return endpoint;
+        }
+    }
+}
