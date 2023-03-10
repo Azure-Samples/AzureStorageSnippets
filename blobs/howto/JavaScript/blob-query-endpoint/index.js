c5f247b4bfeb1728f8fd1bc009ef2173bb19c383
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2023-03-10 10:47:52 -0500

    Update JS sample

--- blobs/howto/JavaScript/blob-query-endpoint/index.js
+++ blobs/howto/JavaScript/blob-query-endpoint/index.js
@@ -1,3 +1,4 @@
+const { ResourceManagementClient } = require("@azure/arm-resources");
 const { StorageManagementClient } = require("@azure/arm-storage");
 const { DefaultAzureCredential } = require("@azure/identity");
 const { BlobServiceClient } = require("@azure/storage-blob");
@@ -7,16 +8,26 @@ async function getBlobServiceEndpoint(saName, credential) {
   const subscriptionId = "<subscription-id>";
   const rgName = "<resource-group-name>";
 
-  const client = new StorageManagementClient(
+  const resourceMgmtClient = new ResourceManagementClient(
     credential,
     subscriptionId
   );
 
-  const storageAccount = await client.storageAccounts.getProperties(
+  // Register the Storage resource provider in the subscription
+  resourceMgmtClient.providers.register("Microsoft.Storage");
+
+  const storageMgmtClient = new StorageManagementClient(
+    credential,
+    subscriptionId
+  );
+
+  // Get the properties for the specified storage account
+  const storageAccount = await storageMgmtClient.storageAccounts.getProperties(
     rgName,
     saName
   );
 
+  // Get the primary endpoint for the blob service
   const endpoint = storageAccount.primaryEndpoints.blob;
 
   return endpoint;
@@ -25,14 +36,17 @@ async function getBlobServiceEndpoint(saName, credential) {
 
 async function main() {
   // <Snippet_create_client_with_endpoint>
-  // For client-side applications running in the browser, use InteractiveBrowserCredential instead of DefaultAzureCredential. See https://aka.ms/azsdk/js/identity/examples for more details.
+  // For client-side applications running in the browser, use InteractiveBrowserCredential instead of DefaultAzureCredential. 
+  // See https://aka.ms/azsdk/js/identity/examples for more details.
 
   const saName = "<storage-account-name>";
   const credential = new DefaultAzureCredential();
 
+  // Call out to our function that retrieves the blob service endpoint for a storage account
   const endpoint = await getBlobServiceEndpoint(saName, credential)
   console.log(endpoint);
 
+  // Now that we know the endpoint, create the client object
   const blobServiceClient = new BlobServiceClient(
     endpoint,
     credential);
