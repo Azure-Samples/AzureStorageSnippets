291e6ac40d9d86bd3c28a92d0da467ce555ab686
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2023-03-14 11:02:43 -0400

    Edits

--- blobs/howto/dotnet/BlobDevGuideBlobs/Program.cs
+++ blobs/howto/dotnet/BlobDevGuideBlobs/Program.cs
@@ -5,7 +5,7 @@ using BlobDevGuide;
 
 // TODO: Replace <storage-account-name> with your actual storage account name
 var blobServiceClient = new BlobServiceClient(
-        new Uri("https://pjstorageaccounttest.blob.core.windows.net"),
+        new Uri("https://<storage-account-name>.blob.core.windows.net"),
         new DefaultAzureCredential());
 
 await CopyBlob.CopyBlobAsync(blobServiceClient);
