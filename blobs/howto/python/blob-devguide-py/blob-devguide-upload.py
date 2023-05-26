5d5ea44b8085b635c227502ad9cb45652dc3c6f1
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2023-05-26 16:23:17 -0400

    Add py upload sample with transfer options

--- blobs/howto/python/blob-devguide-py/blob-devguide-upload.py
+++ blobs/howto/python/blob-devguide-py/blob-devguide-upload.py
@@ -2,12 +2,12 @@ import io
 import os
 import uuid
 from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient, ContainerClient, BlobBlock
+from azure.storage.blob import BlobServiceClient, ContainerClient, BlobBlock, BlobClient
 
 class BlobSamples(object):
 
     # <Snippet_upload_blob_data>
-    def upload_blob_data(self, blob_service_client: BlobServiceClient, container_name):
+    def upload_blob_data(self, blob_service_client: BlobServiceClient, container_name: str):
         blob_client = blob_service_client.get_blob_client(container=container_name, blob="sample-blob.txt")
         data = b"Sample data for blob"
 
@@ -16,21 +16,21 @@ class BlobSamples(object):
     # </Snippet_upload_blob_data>
 
     # <Snippet_upload_blob_stream>        
-    def upload_blob_stream(self, blob_service_client: BlobServiceClient, container_name):
+    def upload_blob_stream(self, blob_service_client: BlobServiceClient, container_name: str):
         blob_client = blob_service_client.get_blob_client(container=container_name, blob="sample-blob.txt")
         input_stream = io.BytesIO(os.urandom(15))
         blob_client.upload_blob(input_stream, blob_type="BlockBlob")
     # </Snippet_upload_blob_stream>
 
     # <Snippet_upload_blob_file>
-    def upload_blob_file(self, blob_service_client: BlobServiceClient, container_name):
+    def upload_blob_file(self, blob_service_client: BlobServiceClient, container_name: str):
         container_client = blob_service_client.get_container_client(container=container_name)
         with open(file=os.path.join('filepath', 'filename'), mode="rb") as data:
             blob_client = container_client.upload_blob(name="sample-blob.txt", data=data, overwrite=True)
     # </Snippet_upload_blob_file>
 
     # <Snippet_upload_blob_tags>
-    def upload_blob_tags(self, blob_service_client: BlobServiceClient, container_name):
+    def upload_blob_tags(self, blob_service_client: BlobServiceClient, container_name: str):
         container_client = blob_service_client.get_container_client(container=container_name)
         sample_tags = {"Content": "image", "Date": "2022-01-01"}
         with open(file=os.path.join('filepath', 'filename'), mode="rb") as data:
@@ -58,6 +58,22 @@ class BlobSamples(object):
             blob_client.commit_block_list(block_id_list)
     # </Snippet_upload_blob_blocks>
 
+    # <Snippet_upload_blob_transfer_options>
+    def upload_blob_transfer_options(self, account_url: str, container_name: str, blob_name: str):
+        # Create a BlobClient object with data transfer options for upload
+        blob_client = BlobClient(
+            account_url=account_url, 
+            container_name=container_name, 
+            blob_name=blob_name,
+            credential=DefaultAzureCredential(),
+            max_block_size=1024*1024*4,
+            max_single_put_size=1024*1024*8,
+        )
+        
+        with open(file=os.path.join(r'file_path', blob_name), mode="rb") as data:
+            blob_client = blob_client.upload_blob(data=data, overwrite=True, max_concurrency=1)
+    # </Snippet_upload_blob_transfer_options>
+
 if __name__ == '__main__':
     # TODO: Replace <storage-account-name> with your actual storage account name
     account_url = "https://<storage-account-name>.blob.core.windows.net"
@@ -71,9 +87,10 @@ if __name__ == '__main__':
 
     file_path = os.path.join(r'file_path', 'file_name')
     block_size = 1024*1024*4
-    sample.upload_blocks(container_client, file_path, block_size)
+    #sample.upload_blocks(container_client, file_path, block_size)
 
     #sample.upload_blob_data(blob_service_client, "sample-container")
     #sample.upload_blob_stream(blob_service_client, "sample-container")
     #sample.upload_blob_file(blob_service_client, "sample-container")
     #sample.upload_blob_tags(blob_service_client, "sample-container")
+    sample.upload_blob_transfer_options(account_url, "sample-container", "sample-blob.txt")
