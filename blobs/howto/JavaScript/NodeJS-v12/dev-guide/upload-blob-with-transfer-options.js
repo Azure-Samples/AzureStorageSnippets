b785b44bb7a3e1a3212bb63263a10e9c88a2a269
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2023-06-19 15:48:50 -0400

    Edits for JS upload samples

--- blobs/howto/JavaScript/NodeJS-v12/dev-guide/upload-blob-with-transfer-options.js
+++ blobs/howto/JavaScript/NodeJS-v12/dev-guide/upload-blob-with-transfer-options.js
@@ -17,7 +17,7 @@ async function uploadWithTransferOptions(containerClient, blobName, localFilePat
   // Specify index tags for blob
   const uploadOptions = {
     // Specify data transfer options
-    blockSize: 4 * 1024 * 1024, // 4 Mib max block size
+    blockSize: 4 * 1024 * 1024, // 4 MiB max block size
     concurrency: 2, // maximum number of parallel transfer workers
     maxSingleShotSize: 8 * 1024 * 1024, // 8 MiB initial transfer size
   } 
