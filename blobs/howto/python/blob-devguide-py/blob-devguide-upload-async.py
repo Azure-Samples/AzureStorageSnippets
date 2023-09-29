ec3e96432e9fd6fdb3d64b3e495af3220fbc21c1
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2023-09-29 13:18:11 -0400

    Update client creation for async

--- blobs/howto/python/blob-devguide-py/blob-devguide-upload-async.py
+++ blobs/howto/python/blob-devguide-py/blob-devguide-upload-async.py
@@ -99,23 +99,21 @@ async def main():
     account_url = "https://<storage-account-name>.blob.core.windows.net"
     credential = DefaultAzureCredential()
 
-    # Create the BlobServiceClient object
-    blob_service_client = BlobServiceClient(account_url, credential=credential)
-    container_client = blob_service_client.get_container_client(container="sample-container")
-
     sample = BlobSamples()
 
-    file_path = os.path.join(r'file_path', 'file_name')
-    block_size = 1024*1024*4 # 4 MiB
-    #await sample.upload_blocks(container_client, file_path, block_size)
+    async with BlobServiceClient(account_url, credential=credential) as blob_service_client:
+        await sample.upload_blob_data(blob_service_client, "sample-container")
+        #await sample.upload_blob_stream(blob_service_client, "sample-container")
+        #await sample.upload_blob_file(blob_service_client, "sample-container")
+        #await sample.upload_blob_tags(blob_service_client, "sample-container")
+        #await sample.upload_blob_transfer_options(account_url, "sample-container", "sample-blob.txt")
+        #await sample.upload_blob_access_tier(blob_service_client, "sample-container", "sample-blob.txt")
+        #await sample.upload_blob_transfer_validation(blob_service_client, "sample-container", "sample-blob.txt")
 
-    await sample.upload_blob_data(blob_service_client, "sample-container")
-    #await sample.upload_blob_stream(blob_service_client, "sample-container")
-    #await sample.upload_blob_file(blob_service_client, "sample-container")
-    #await sample.upload_blob_tags(blob_service_client, "sample-container")
-    #await sample.upload_blob_transfer_options(account_url, "sample-container", "sample-blob.txt")
-    #await sample.upload_blob_access_tier(blob_service_client, "sample-container", "sample-blob.txt")
-    #await sample.upload_blob_transfer_validation(blob_service_client, "sample-container", "sample-blob.txt")
+        container_client = blob_service_client.get_container_client(container="sample-container")
+        #file_path = os.path.join(r'file_path', 'file_name')
+        #block_size = 1024*1024*4 # 4 MiB
+        #await sample.upload_blocks(container_client, file_path, block_size)
 
 if __name__ == '__main__':
     asyncio.run(main())
