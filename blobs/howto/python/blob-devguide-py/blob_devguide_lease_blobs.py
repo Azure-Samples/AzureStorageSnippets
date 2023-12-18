b9fbf8e70b95331450578c7fe971065b51266260
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2023-12-18 12:24:33 -0500

    Async samples for lease operations

---
+++ blobs/howto/python/blob-devguide-py/blob_devguide_lease_blobs.py
@@ -0,0 +1,48 @@
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobLeaseClient 
+
+class BlobSamples(object):
+
+    # <Snippet_acquire_blob_lease>
+    def acquire_blob_lease(self, blob_service_client: BlobServiceClient, container_name):
+        # Instantiate a BlobClient
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob="sample-blob.txt")
+
+        # Acquire a 30-second lease on the blob
+        lease_client = blob_client.acquire_lease(30)
+
+        return lease_client
+    # </Snippet_acquire_blob_lease>
+
+    # <Snippet_renew_blob_lease>
+    def renew_blob_lease(self, lease_client: BlobLeaseClient):
+        # Renew a lease on a blob
+        lease_client.renew()
+    # </Snippet_renew_blob_lease>
+
+    # <Snippet_release_blob_lease>
+    def release_blob_lease(self, lease_client: BlobLeaseClient):
+        # Release a lease on a blob
+        lease_client.release()
+    # </Snippet_release_blob_lease>
+
+    # <Snippet_break_blob_lease>
+    def break_blob_lease(self, lease_client: BlobLeaseClient):
+        # Break a lease on a blob
+        lease_client.break_lease()
+    # </Snippet_break_blob_lease>
+
+if __name__ == '__main__':
+    # TODO: Replace <storage-account-name> with your actual storage account name
+    account_url = "https://<storage-account-name>.blob.core.windows.net"
+    credential = DefaultAzureCredential()
+
+    # Create the BlobServiceClient object
+    blob_service_client = BlobServiceClient(account_url, credential=credential)
+
+    sample = BlobSamples()
+
+    lease_client = sample.acquire_blob_lease(blob_service_client, "sample-container")
+    sample.renew_blob_lease(lease_client)
+    sample.release_blob_lease(lease_client)
+    sample.break_blob_lease(lease_client)
---
+++ blobs/howto/python/blob-devguide-py/blob_devguide_lease_container_async.py
@@ -0,0 +1,50 @@
+import asyncio
+from azure.identity.aio import DefaultAzureCredential
+from azure.storage.blob.aio import BlobServiceClient, BlobLeaseClient
+
+class ContainerSamples(object):
+
+    # <Snippet_acquire_container_lease>
+    async def acquire_container_lease(self, blob_service_client: BlobServiceClient, container_name):
+        # Instantiate a ContainerClient
+        container_client = blob_service_client.get_container_client(container=container_name)
+
+        # Acquire a 30-second lease on the container
+        lease_client = BlobLeaseClient(container_client)
+        await lease_client.acquire(lease_duration=30)
+
+        return lease_client
+    # </Snippet_acquire_container_lease>
+
+    # <Snippet_renew_container_lease>
+    async def renew_container_lease(self, lease_client: BlobLeaseClient):
+        # Renew a lease on the container
+        await lease_client.renew()
+    # </Snippet_renew_container_lease>
+
+    # <Snippet_release_container_lease>
+    async def release_container_lease(self, lease_client: BlobLeaseClient):
+        # Release a lease on the container
+        await lease_client.release()
+    # </Snippet_release_container_lease>
+
+    # <Snippet_break_container_lease>
+    async def break_container_lease(self, lease_client: BlobLeaseClient):
+        # Break a lease on the container
+        await lease_client.break_lease()
+    # </Snippet_break_container_lease>
+
+# <Snippet_create_client_async>
+async def main():
+    sample = ContainerSamples()
+
+    # TODO: Replace <storage-account-name> with your actual storage account name
+    account_url = "https://<storage-account-name>.blob.core.windows.net"
+    credential = DefaultAzureCredential()
+
+    async with BlobServiceClient(account_url, credential=credential) as blob_service_client:
+        lease_client = await sample.acquire_container_lease(blob_service_client, "sample-container")
+
+if __name__ == '__main__':
+    asyncio.run(main())
+# </Snippet_create_client_async>
