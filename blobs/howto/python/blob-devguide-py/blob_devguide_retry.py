a8616878548283b143858e850aaa8b8c031d75c0
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2024-04-09 13:34:08 -0400

    Updates

--- blobs/howto/python/blob-devguide-py/blob_devguide_retry.py
+++ blobs/howto/python/blob-devguide-py/blob_devguide_retry.py
@@ -4,21 +4,25 @@ from azure.storage.blob import BlobServiceClient, ExponentialRetry, LinearRetry
 class RetrySamples(object):
 
     def retry_policy_exponential(self):
+        # <Snippet_retry_exponential>
         # Specify retry policy parameters
         retry = ExponentialRetry(initial_backoff=10, increment_base=4, max_attempts=5)
 
         # Create the BlobServiceClient object
         blob_service_client = BlobServiceClient(account_url, credential=credential, retry_policy=retry)
+        # </Snippet_retry_exponential>
 
         return blob_service_client
 
     def retry_policy_linear(self):
+        # <Snippet_retry_linear>
         # Specify retry policy parameters
         retry = LinearRetry(backoff=10, max_attempts=5, retry_to_secondary=True)
 
         # Create the BlobServiceClient object
         blob_service_client = BlobServiceClient(account_url, credential=credential, retry_policy=retry)
-
+        # </Snippet_retry_linear>
+        
         return blob_service_client
 
 
@@ -29,7 +33,9 @@ if __name__ == '__main__':
 
     sample = RetrySamples()
 
-    sample.retry_policy_exponential()
-    sample.retry_policy_linear()
+    client_exp = sample.retry_policy_exponential()
+    client_lin = sample.retry_policy_linear()
+
+    # Do something with the client
 
     
