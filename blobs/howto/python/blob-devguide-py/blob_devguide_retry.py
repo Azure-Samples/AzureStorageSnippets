c294e28427a19598566a7039c674e623263b4018
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2024-04-15 08:59:46 -0400

    Update py retry samples

--- blobs/howto/python/blob-devguide-py/blob_devguide_retry.py
+++ blobs/howto/python/blob-devguide-py/blob_devguide_retry.py
@@ -5,8 +5,12 @@ class RetrySamples(object):
 
     def retry_policy_default(self):
         # <Snippet_retry_default>
-        # Create the BlobServiceClient object
-        blob_service_client = BlobServiceClient(account_url, credential=credential, retry_total=5,
+        # TODO: Replace <storage-account-name> with your actual storage account name
+        account_url = "https://<storage-account-name>.blob.core.windows.net"
+        credential = DefaultAzureCredential()
+
+        # Create the BlobServiceClient object with retry options
+        blob_service_client = BlobServiceClient(account_url, credential, retry_total=5,
                                                 retry_connect=5, retry_read=5, retry_status=5)
         # </Snippet_retry_default>
 
@@ -14,32 +18,36 @@ class RetrySamples(object):
 
     def retry_policy_exponential(self):
         # <Snippet_retry_exponential>
+        # TODO: Replace <storage-account-name> with your actual storage account name
+        account_url = "https://<storage-account-name>.blob.core.windows.net"
+        credential = DefaultAzureCredential()
+
         # Specify retry policy parameters
         retry = ExponentialRetry(initial_backoff=10, increment_base=4, retry_total=3)
 
         # Create the BlobServiceClient object
-        blob_service_client = BlobServiceClient(account_url, credential=credential, retry_policy=retry)
+        blob_service_client = BlobServiceClient(account_url, credential, retry_policy=retry)
         # </Snippet_retry_exponential>
 
         return blob_service_client
 
     def retry_policy_linear(self):
         # <Snippet_retry_linear>
+        # TODO: Replace <storage-account-name> with your actual storage account name
+        account_url = "https://<storage-account-name>.blob.core.windows.net"
+        credential = DefaultAzureCredential()
+
         # Specify retry policy parameters
         retry = LinearRetry(backoff=10, retry_total=3, retry_to_secondary=True)
 
         # Create the BlobServiceClient object
-        blob_service_client = BlobServiceClient(account_url, credential=credential, retry_policy=retry)
+        blob_service_client = BlobServiceClient(account_url, credential, retry_policy=retry)
         # </Snippet_retry_linear>
 
         return blob_service_client
 
 
 if __name__ == '__main__':
-    # TODO: Replace <storage-account-name> with your actual storage account name
-    account_url = "https://<storage-account-name>.blob.core.windows.net"
-    credential = DefaultAzureCredential()
-
     sample = RetrySamples()
 
     client_def = sample.retry_policy_default()
